7cc277094469c2dcfed3483a55e35458ab33d40c
theaazam <thecleanviews@gmail.com>
2025-11-13 16:36:31 +0530

    Fixed export default in layout and page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-// src/app/layout.jsx
+// src/app/layout.js
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>MySkillTools</title>
-      </head>
-      <body style={{ margin: 0, fontFamily: 'Arial, sans-serif' }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
